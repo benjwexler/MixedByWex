ebde50d0c7e377ce51e87bb18c309c99a79e0dbf
Ben Wexler <mixedywex@gmail.com>
2018-10-19 18:33:41 -0400

    Divs are now created dynamically

--- main.js
+++ main.js
@@ -21,4 +21,43 @@ let tracksObjLength = Object.keys(tracksObj).length
 for (let i = 1; i <= tracksObjLength; i++) {
 
     tracksObj[`${i}`].isPlaying = false
+
+    const alltracksBossContainer = document.getElementById("alltracksBossContainer")
+    var tracksContainer = document.createElement("div");
+    tracksContainer.classList.add("tracksContainer");
+    alltracksBossContainer.appendChild(tracksContainer);
+
+    var coverArtContainer = document.createElement("div");
+    coverArtContainer.classList.add("coverArtContainer");
+    tracksContainer.appendChild(coverArtContainer);
+    var coverArtPic = document.createElement("img");
+    if (i===2) {
+        coverArtPic.classList.add("coverArt");  
+    }
+    coverArtPic.classList.add("iGotYouCoverArt");
+    coverArtContainer.appendChild(coverArtPic);
+
+    var trackTitleContainer = document.createElement("div");
+    trackTitleContainer.classList.add("trackTitleContainer");
+    tracksContainer.appendChild(trackTitleContainer);
+    var title = document.createElement("div");
+    title.classList.add("title");
+    trackTitleContainer.appendChild(title);
+
+    var playPauseContainer = document.createElement("div");
+    playPauseContainer.classList.add("playPauseContainer");
+    tracksContainer.appendChild(playPauseContainer);
+    var fontAwesomePlayTrackIcon = document.createElement("i");
+    fontAwesomePlayTrackIcon.classList.add("fas", "fa-play", "fa-2x", "fontAwesomePlayTrackIcon");
+    playPauseContainer.appendChild(fontAwesomePlayTrackIcon);
+}
+
+// const tracks is an array of how many different songs I will be displaying on the screen
+const tracks = document.getElementsByClassName("tracksContainer")
+
+for (let i = 0; i < tracks.length; i++) {
+    window[`track${i+1}`] = tracks[i]
+    window[`track${i+1}CoverArt`] = window[`track${i+1}`].querySelector(".coverArtContainer").querySelector(".iGotYouCoverArt").src = tracksObj[`${i+1}`].coverArt
+    window[`track${i+1}Title`] = window[`track${i+1}`].querySelector(".trackTitleContainer").querySelector(".title").innerText = tracksObj[`${i+1}`].name
+    
 }
